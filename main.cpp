29faa63ccd4f24d00da61418e0196af5da02e896
stanislav200 <stasden17@gmail.com>
2024-05-23 00:00:38 +0300

    Сервер Был создан сервер, добавлена проверка на игроков

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include "argparse.hpp"
+#include "server.hpp"
 
 
 int main(int argc, char *argv[])
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
         is_server = true;
     }
     char* port = getCmdOption(argv, argv + argc, "--port");
-    if (!port){
+    if (port == nullptr){
         std::cerr << "Please enter the port!" << std::endl;
         return 1;
     }
@@ -23,6 +24,18 @@ int main(int argc, char *argv[])
     if(!is_server & !ip){
         std::cerr << "Please enter ip, to connect to server" << std::endl;
         return 1;
+    }
+    char* num_connections = getCmdOption(argv, argv + argc, "--players");
+    if (!num_connections){
+        std::cerr << "Please enter number of players" << std::endl;
+        return 1;
+    }
+    else if (atoi(num_connections) > 4 || atoi(num_connections) < 0){
+        std::cerr << "Please enter number of players between 2 and 4" << std::endl;
+        return 1;
+    }
+    if (is_server){
+        server_work(atoi(port), atoi(num_connections));
     }
     return 0;
 }
--- server.hpp
+++ server.hpp
@@ -1,9 +1,39 @@
-#include <ncurses.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <vector>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+using namespace std;
 
 
-int create_server(){
+int server_work(int port, int num_connections){
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // Создаем сокет
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        cerr << "socket failed";
+        exit(EXIT_FAILURE);
+    }
+
+    // Устанавливаем адрес и порт сервера
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    // Bind сокета к адресу и порту
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        cerr << "bind failed";
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+            cerr << "listen failed";
+            exit(EXIT_FAILURE);
+    }
+    vector<int> client_sockets;
     return 0;
-}
+} 
